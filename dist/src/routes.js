fbf30fc419790d63445f10d39dc0ce1d86257176
Pratham More <156173075+prathammore0025@users.noreply.github.com>
2025-05-21 10:55:13 +0530

    Delete dist directory

--- dist/src/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Example route
-router.get('/hello', (req, res) => {
-  res.json({ message: 'Hello, World!' });
-});
-
-module.exports = router;
